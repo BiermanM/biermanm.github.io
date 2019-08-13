64618982c2bc3fe72d73b504001fbd0d780b4eba
Matthew Bierman <matthew.bierman2@gmail.com>
2019-08-13 02:52:37 -0400

    All of new website

---
+++ js/projects.js
@@ -0,0 +1,7 @@
+// on page load, smooth scroll to first item
+$(function(){
+    $('html, body').animate({
+        scrollTop: $(".project > .item:nth-child(1)").offset().top - ((window.innerHeight - $(".project > .item:nth-child(1)").height()) / 2)
+    }, 800);
+    return false;
+});
